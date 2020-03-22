efbab7ea4400508fda6b7df8a5350c8540b96814
Yauheni Bandyukevich <zhenya.bandyukevich@mail.ru>
2020-03-22 21:13:15 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0; i < bracketsConfig.length; i++) {
+       let brackets = bracketsConfig[i].join('');
+       if (str.includes(brackets)) {
+           str = str.replace(brackets, '');
+           i = -1;
+       }
+   }
+  return  str === '' ? true : false;
 }
